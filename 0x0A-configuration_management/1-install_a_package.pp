761a3a73af75d8db96fb589605b08cd5b8385039
Yousef eslam <yousefeslam214@gmail.com>
2024-04-19 11:29:20 +0200

    1-task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# File: 1-install_a_package.pp
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
