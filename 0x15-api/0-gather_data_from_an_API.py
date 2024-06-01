09cf7cc83011e3bb2c8404bc049ee7d21d812d96
Yousef eslam <yousefeslam214@gmail.com>
2024-06-01 18:20:23 +0300

    0-task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information
-about their TODO list progress"""
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
+
     userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
 
-    if user.status_code == 200:
-        name = user.json().get('name')
-        todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-        totalTasks = 0
-        completed = 0
-
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                totalTasks += 1
-                if task.get('completed'):
-                    completed += 1
-
-		print('Employee {} is done with tasks({}/{}):'
-        .format(name, completed, totalTasks))
-        for task in todos.json():
-            if task.get('userId') == int(userId) and task.get('completed'):
-                print("\t" + task.get('title'))
-    else:
-        print('Request failed with status code:', user.status_code)
-
     name = user.json().get('name')
+
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     totalTasks = 0
     completed = 0
@@ -40,3 +22,9 @@ if __name__ == "__main__":
             totalTasks += 1
             if task.get('completed'):
                 completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
