f640f7d3a304486fd0d5f46f6717b80bdd9d6f14
Yousef eslam <yousefeslam214@gmail.com>
2024-06-01 18:51:59 +0300

    1-task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    filename = userId + '.csv'
+
+    # Write data to CSV file
+    with open(filename, mode='w') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL, lineterminator='\n')
+
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId , name, str(task.get('completed')), task.get('title')])
