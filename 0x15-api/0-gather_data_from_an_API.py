c6b152d0473290e1aa6d669b1f6f798785b045ec
Yousef eslam <yousefeslam214@gmail.com>
2024-06-01 18:06:35 +0300

    0-task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,27 +7,30 @@ import sys
 import requests
 import sys
 
-# import requests
 
-# import urllib.request
-
-# Rest of your code goes here
-# Make a GET request to a URL
-# response = requests.get('https://api.example.com')
-
-# # Check if the request was successful (status code 200)
-# if response.status_code == 200:
-#     # Print the response content
-#     print(response.text)
-# else:
-#     print('Request failed with status code:', response.status_code)
-    
 if __name__ == "__main__":
 
 
     userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    if user.status_code == 200:
+        name = user.json().get('name')
+        todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+        totalTasks = 0
+        completed = 0
 
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                totalTasks += 1
+                if task.get('completed'):
+                    completed += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(name, completed, totalTasks))
+        for task in todos.json():
+            if task.get('userId') == int(userId) and task.get('completed'):
+                print("\t" + task.get('title'))
+    else:
+        print('Request failed with status code:', user.status_code)
     name = user.json().get('name')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     totalTasks = 0
