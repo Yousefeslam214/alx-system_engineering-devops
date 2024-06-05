c3c4bb500a88bdb0af0084111aa60e99e4bc2caf
Yousef eslam <yousefeslam214@gmail.com>
2024-06-05 19:00:40 +0300

    0-task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,14 @@
 """ Task-0 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-	""" fun """
-	sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={"User-Agent": "My-User-Agent"},allow_redirects=False)
-	if sub_info.status_code >= 300:
-		return 0
-	return sub_info.json().get("data").get("subscribers")
-
-
-
-# CLIENT_ID = 't-05GuiL8mVRS5xXimyk4Q'
-# CLIENT_SECRET = 'Egj2TyLBT5jzyIsfi8h2sWDUq7IK8g'
+    """Returns the number of subscribers for a given subreddit."""
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
+    if sub_info.status_code >= 300:
+        return 0
+    return sub_info.json().get("data").get("subscribers")
